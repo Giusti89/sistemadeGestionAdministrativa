527484687d47485e38744734d4992fab30441ea9
Giusti89 <giustiever17@gmail.com>
2025-07-24 14:45:34 -0400

    aspecto visual formulario trabajo nuevo botones

--- resources/views/insumos/show.blade.php
+++ resources/views/insumos/show.blade.php
@@ -2,52 +2,55 @@
 
     <div class="contenedor">
         <div class="tabla tabla1">
-                    <h1>Insumos</h1>
-                    <div class="table-container">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Cantidad</th>
-                                    <th>Insumos</th>
-                                    <th>Detalle</th>
-                                    <th>Costo</th>
-                                    
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($insumos as $trab)
-                                    <tr>
-                                        <td class="filas-tabla">
-                                            {{ $trab->cantidad }}
-                                        </td>
-                                        <td class="filas-tabla">
-                                            {{ $trab->nombre }}
-                                        </td>
-                                        <td class="filas-tabla">
-                                            {{ $trab->detalle }}
-                                        </td>
-                                        <td class="filas-tabla">
-                                            {{ $trab->costo }}
-                                        </td>
-                                        
-                                        
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th>Costo</th>
-                                    <td class="filas-tabla" colspan="1"></td>
-                                    <td class="filas-tabla" colspan="1"></td>
-                                    <td class="filas-tabla" colspan="1"> {{ $costoprod }} Bs.</td>
-                                    
-                                </tr>
-                            </tfoot>
-                        </table>
-                        
-                    </div>
-                   
+            <h1>Insumos</h1>
+            <div class="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Cantidad</th>
+                            <th>Insumos</th>
+                            <th>Detalle</th>
+                            <th>Costo</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($insumos as $trab)
+                            <tr>
+                                <td class="filas-tabla">
+                                    {{ $trab->cantidad }}
+                                </td>
+                                <td class="filas-tabla">
+                                    {{ $trab->nombre }}
+                                </td>
+                                <td class="filas-tabla">
+                                    {{ $trab->detalle }}
+                                </td>
+                                <td class="filas-tabla">
+                                    {{ $trab->costo }}
+                                </td>
+
+
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Costo</th>
+                            <td class="filas-tabla" colspan="1"></td>
+                            <td class="filas-tabla" colspan="1"></td>
+                            <td class="filas-tabla" colspan="1"> {{ $costoprod }} Bs.</td>
+
+                        </tr>
+                    </tfoot>
+                </table>
+                <div class="forget">
+                    <a href="{{ route('trabIndex') }}">Regresar</a>
                 </div>
+
+            </div>
+
+        </div>
     </div>
 
 
